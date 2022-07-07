2462ea4b48582a9ff3e075b06a60c37c31139ea9
zEnesy <zenesy13@gnail.com>
2022-07-07 12:58:29 +0300

    Fucking slave

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,25 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = applicationContext.getBean("helloworld",HelloWorld.class);
+        Cat cat1 = applicationContext.getBean("cat",Cat.class);
+        Cat cat2 = applicationContext.getBean("cat",Cat.class);
+
         System.out.println(bean.getMessage());
+        System.out.println(bean);
+        System.out.println(bean2 + "\n");
+        System.out.println(bean == bean2);
+        System.out.println();
+
+        System.out.println(cat1);
+        System.out.println(cat2 + "\n");
+        System.out.println(cat1 == cat2);
+
+
+
     }
 }
